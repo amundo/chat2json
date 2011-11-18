a62321a4feb591097d1f8fc00fa2dfe41ea0e05a
Patrick Hall <pat@amundo.com>
2011-11-17 20:28:26 -0800

    different repo

--- parse/jsonize_corpus_dir.py
+++ parse/jsonize_corpus_dir.py
@@ -9,8 +9,10 @@ from chat import *
 
 #macwhinney = glob('/Users/pat/Sites/ucsb/courses/2011/lgacq/childes/Eng-USA/MacWhinney/*.cha')
 rollins = glob('/Users/pat/Sites/ucsb/courses/2011/lgacq/childes/Clinical/Rollins/*cha')
+alex = glob('/Users/pat/Sites/ucsb/courses/2011/lgacq/childes/Eng-USA/Providence/Alex/*cha')
 
-for cha_file in rollins:
+
+for cha_file in alex:
   new_json_file = cha_file.replace('.cha', '.json')
   open(new_json_file, 'w').write( json.dumps(parse_transcript(cha_file), indent=2) )
 
